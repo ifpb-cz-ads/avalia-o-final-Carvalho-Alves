e9f4912624057d3d5ee18dbce6778d7e2591c822
Pedro Henrique PC <carvalho280928@gmail.com>
2023-12-22 19:24:28 -0300

    Colocando videos certos e mexendo no codigo do banco

--- src/main/java/Supermercado/dao/Conexao.java
+++ src/main/java/Supermercado/dao/Conexao.java
@@ -43,7 +43,7 @@ use supermercado;
 CREATE TABLE usuario (
     codUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL
 );
 
